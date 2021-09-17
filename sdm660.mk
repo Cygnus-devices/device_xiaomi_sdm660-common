a6d9d9d46643b5fe253a4566f41b0c3d3c8ff00c
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2021-09-17 11:38:39 +0530

    sdm660-common: adapt to CygnusOS
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -383,12 +383,8 @@ PRODUCT_PACKAGES += \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
+    qti_telephony_utils.xml 
+    
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
